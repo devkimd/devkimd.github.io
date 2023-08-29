dca5d91540871c25427f31be94802925190d6c5e
KimDongyoung <dongyoung.dev@gmail.com>
2023-08-30 01:19:32 +0900

    Gatsby 튜토리얼 파트 3 완료
    - 정적 이미지 추가를 위한 플러그인 설치 및 설정
    - 이미지 업로드

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout.js'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src="../images/icon.png"
+        alt="gatsby icon"
+      />
     </Layout>
   )
 }
